639946cc26dc0512fc1e68c8af8929e01953a931
Ashish Deshpande <232292705+ashd90@users.noreply.github.com>
2025-10-13 08:53:45 +0530

    updated file

--- case_with_read.sh
+++ case_with_read.sh
@@ -4,7 +4,8 @@ echo "Please choose an option:"
 echo "1. Display current date and time"
 echo "2. List files in current directory"
 echo "3. Print system uptime"
-echo "4. Exit"
+echo "4. Print system information"
+echo "5. Exit"
 
 read -p "Enter your choice (1-4): " choice
 
@@ -21,9 +22,14 @@ case $choice in
         uptime
         ;;
     4)
+        echo "System information."
+        fastfetch
+        ;;
+    5)
         echo "Exiting script."
         exit 0
         ;;
+
     *)
         echo "Invalid choice. Please enter a number between 1 and 4."
         ;;
